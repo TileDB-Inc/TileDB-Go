4b02cf562145cbfce4a14f6b6982922b4f9e40bf
Tyler Denniston <denniston.t@gmail.com>
2019-05-14 12:39:48 -0400

    Add Datatype.MakeSlice function to API

--- enums.go
+++ enums.go
@@ -9,7 +9,11 @@ package tiledb
 */
 import "C"
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
 // ArrayType enum for tiledb arrays
 type ArrayType int8
@@ -110,6 +114,54 @@ func (d Datatype) ReflectKind() reflect.Kind {
 // Size returns the datatype size in bytes
 func (d Datatype) Size() uint64 {
 	return uint64(C.tiledb_datatype_size(C.tiledb_datatype_t(d)))
+}
+
+// MakeSlice makes a slice of the correct type corresponding to the datatype, with a given number of elements
+func (d Datatype) MakeSlice(numElements uint64) (interface{}, unsafe.Pointer, error) {
+	switch d {
+	case TILEDB_INT8:
+		slice := make([]int8, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_INT16:
+		slice := make([]int16, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_INT32:
+		slice := make([]int32, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_INT64:
+		slice := make([]int64, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_UINT8, TILEDB_CHAR, TILEDB_STRING_ASCII, TILEDB_STRING_UTF8:
+		slice := make([]uint8, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_UINT16, TILEDB_STRING_UTF16, TILEDB_STRING_UCS2:
+		slice := make([]uint16, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_UINT32, TILEDB_STRING_UTF32, TILEDB_STRING_UCS4:
+		slice := make([]uint32, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_UINT64:
+		slice := make([]uint64, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_FLOAT32:
+		slice := make([]float32, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	case TILEDB_FLOAT64:
+		slice := make([]float64, numElements)
+		return slice, unsafe.Pointer(&slice[0]), nil
+
+	default:
+		return nil, nil, fmt.Errorf("error making datatype slice; unrecognized datatype: %d", d)
+	}
 }
 
 // EncryptionType represents different encryption algorithms
