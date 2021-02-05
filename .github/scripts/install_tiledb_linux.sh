db1bceeb01cc79146f85ef7defc521673c57358a
Andreas Ntalakas <antalakas@gmail.com>
2021-02-05 17:55:44 +0200

    bump version of TileDB to 2.2.3 in CI

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,4 +1,4 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.2.1/tiledb-linux-2.2.1-4744a3f-full.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.2.3/tiledb-linux-2.2.3-dbaf5ff-full.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
