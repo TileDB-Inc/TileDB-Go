d07b04f2638b4e3a34ff5b5f5414829c2b7952e4
Seth Shelnutt <Shelnutt2@gmail.com>
2021-03-15 13:19:49 -0400

    Add additional stats functions.
    This allows returning TileDB Statistics as a string and also additional
    functions for returning or dumping the raw statistics which are in JSON
    form.

--- stats.go
+++ stats.go
@@ -77,3 +77,77 @@ func StatsDump(path string) error {
 	}
 	return nil
 }
+
+// Stats returns internal stats as string
+func Stats() (string, error) {
+	var msg *C.char
+
+	// Dump stats to string
+	ret := C.tiledb_stats_dump_str(&msg)
+	if ret != C.TILEDB_OK {
+		return "", fmt.Errorf("Error dumping stats to string")
+	}
+	s := C.GoString(msg)
+
+	ret = C.tiledb_stats_free_str(&msg)
+	if ret != C.TILEDB_OK {
+		return "", fmt.Errorf("Error freeing string from dumping stats to string")
+	}
+
+	return s, nil
+}
+
+// StatsRawDumpSTDOUT prints internal raw (json) stats to stdout
+func StatsRawDumpSTDOUT() error {
+	ret := C.tiledb_stats_raw_dump(C.stdout)
+	if ret != C.TILEDB_OK {
+		return fmt.Errorf("Error dumping stats to stdout")
+	}
+	return nil
+}
+
+// StatsRawDump prints internal raw (json) stats to file path given
+func StatsRawDump(path string) error {
+
+	if _, err := os.Stat(path); err == nil {
+		return fmt.Errorf("Error path already %s exists", path)
+	}
+
+	// Convert to char *
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
+
+	// Set mode as char*
+	cMode := C.CString("w")
+	defer C.free(unsafe.Pointer(cMode))
+
+	// Open file to get FILE*
+	cFile := C.fopen(cPath, cMode)
+	defer C.fclose(cFile)
+
+	// Dump stats to file
+	ret := C.tiledb_stats_raw_dump(cFile)
+	if ret != C.TILEDB_OK {
+		return fmt.Errorf("Error dumping stats to file %s", path)
+	}
+	return nil
+}
+
+// StatsRaw returns internal raw (json) stats as string
+func StatsRaw() (string, error) {
+	var msg *C.char
+
+	// Dump stats to string
+	ret := C.tiledb_stats_raw_dump_str(&msg)
+	if ret != C.TILEDB_OK {
+		return "", fmt.Errorf("Error dumping raw stats to string")
+	}
+	s := C.GoString(msg)
+
+	ret = C.tiledb_stats_free_str(&msg)
+	if ret != C.TILEDB_OK {
+		return "", fmt.Errorf("Error freeing string from dumping raw stats to string")
+	}
+
+	return s, nil
+}
