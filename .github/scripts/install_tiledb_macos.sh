582a1ac471017c603858fd3ec4d141d369423ba0
Stefan Naglee <stefannaglee@gmail.com>
2021-06-03 11:21:26 -0400

    Update CI to use RC

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.2.9/tiledb-macos-2.2.9-dc3bb54-full.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.3.0-rc0/tiledb-macos-2.3.0-rc0-32eb192-full.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
