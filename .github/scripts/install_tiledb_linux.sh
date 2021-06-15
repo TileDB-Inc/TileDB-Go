3e61125978b140d0994251e094b825bd0ee20a4a
Stefan Naglee <stefannaglee@gmail.com>
2021-06-15 14:32:37 -0400

    Move from RC to official release

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,4 +1,4 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.3.0-rc0/tiledb-linux-2.3.0-rc0-32eb192-full.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.3.0/tiledb-linux-2.3.0-a87da7f-full.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
--- .github/scripts/install_tiledb_linux_debug.sh
+++ .github/scripts/install_tiledb_linux_debug.sh
@@ -1,5 +1,5 @@
 set -e -x
-git clone https://github.com/TileDB-Inc/TileDB.git -b 2.3.0-rc0
+git clone https://github.com/TileDB-Inc/TileDB.git -b 2.3.0
 cd TileDB
 mkdir build && cd build
 cmake -DSANITIZER=leak -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
