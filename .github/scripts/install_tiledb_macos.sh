79d3bafc17bde61feb81269c175e1c20d157c9ec
Stefan Naglee <stefannaglee@gmail.com>
2021-09-10 12:01:57 -0400

    Update to 2.4

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.3.4/tiledb-macos-x86_64-2.3.4-e19855e.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.4.0/tiledb-macos-x86_64-2.4.0-baf64e1.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
