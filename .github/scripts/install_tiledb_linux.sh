2a7994a8bc421dd25189a6be9b760fb67473eff4
Andreas Ntalakas <antalakas@gmail.com>
2021-11-22 11:01:16 +0200

    updates to TileDB 2.5.1

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,4 +1,4 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.5.0/tiledb-linux-x86_64-2.5.0-d11292d.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.5.1/tiledb-linux-x86_64-2.5.1-5b65a96.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
