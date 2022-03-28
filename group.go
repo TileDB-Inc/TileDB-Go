56107e05eda66e33fc2859ca30badf08a679ec9f
Stefan Naglee <stefannaglee@gmail.com>
2022-03-28 17:59:54 -0400

    Add member functions

--- group.go
+++ group.go
@@ -83,7 +83,7 @@ func (g *Group) Close() error {
 }
 
 func (g *Group) SetConfig(config *Config) error {
-	ret := C.tiledb_group_set_config(g.context.tiledbContext, g.group, g.config.tiledbConfig)
+	ret := C.tiledb_group_set_config(g.context.tiledbContext, g.group, config.tiledbConfig)
 	if ret != C.TILEDB_OK {
 		return fmt.Errorf("Error setting config on group: %s", g.context.LastError())
 	}
@@ -107,4 +107,56 @@ func (g *Group) Config() (*Config, error) {
 	}
 
 	return &config, nil
+}
+
+func (g *Group) AddMember(uri string, isRelativeURI bool) error {
+	curi := C.CString(uri)
+	defer C.free(unsafe.Pointer(curi))
+	var cRelative C.uint8_t
+	if isRelativeURI {
+		cRelative = 1
+	}
+
+	ret := C.tiledb_group_add_member(g.context.tiledbContext, g.group, curi, cRelative)
+	if ret != C.TILEDB_OK {
+		return fmt.Errorf("Error adding member to group: %s", g.context.LastError())
+	}
+	return nil
+}
+
+func (g *Group) RemoveMember(uri string) error {
+	curi := C.CString(uri)
+	defer C.free(unsafe.Pointer(curi))
+	ret := C.tiledb_group_remove_member(g.context.tiledbContext, g.group, curi)
+	if ret != C.TILEDB_OK {
+		return fmt.Errorf("Error removing member from group: %s", g.context.LastError())
+	}
+	return nil
+}
+
+func (g *Group) GetMemberCount() (uint64, error) {
+	var count C.uint64_t
+	ret := C.tiledb_group_get_member_count(g.context.tiledbContext, g.group, &count)
+	if ret != C.TILEDB_OK {
+		return 0, fmt.Errorf("Error retrieving member count in group: %s", g.context.LastError())
+	}
+	return count, nil
+}
+
+func (g *Group) GetMemberFromIndex(index uint64) (string, ObjectTypeEnum, error) {
+	var curi *C.char
+	defer C.free(unsafe.Pointer(curi))
+
+	var objectTypeEnum C.tiledb_object_t
+	ret := C.tiledb_group_get_member_by_index(g.context.tiledbContext, g.group, C.uint64_t(index), curi, &objectTypeEnum)
+	if ret != C.TILEDB_OK {
+		return "", TILEDB_INVALID, fmt.Errorf("Error getting member by index for group: %s", g.context.LastError())
+	}
+
+	uri := C.GoString(curi)
+	if uri == "" {
+		return "", TILEDB_INVALID, fmt.Errorf("Error getting URI for member %d: uri is empty", index)
+	}
+
+	return uri, ObjectTypeEnum(objectTypeEnum), nil
 }
--- group_test.go
+++ group_test.go
@@ -4,7 +4,9 @@
 package tiledb
 
 import (
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -27,4 +29,130 @@ func TestGroupCreate(t *testing.T) {
 	group, err = NewGroup(context, tmpGroup)
 	require.NoError(t, err)
 	assert.Error(t, group.Create())
+}
+
+func TestGroups_AddMembers(t *testing.T) {
+	tdbCtx, err := NewContext(nil)
+	require.NoError(t, err)
+
+	group, err := createTestGroup(tdbCtx, t.TempDir())
+	require.NoError(t, err)
+
+	// =========================================================================
+	// Test adding members to the group
+	arraySchema := buildArraySchema(tdbCtx, t)
+	require.NoError(t, addTwoArraysToGroup(tdbCtx, group, arraySchema, t.TempDir(), t.TempDir()))
+
+	// verify we have two arrays
+	count, err := memberCount(group)
+	require.NoError(t, err)
+	assert.EqualValues(t, uint(2), count)
+	require.NoError(t, group.Close())
+}
+
+func TestGroups_RemoveMembers(t *testing.T) {
+	tdbCtx, err := NewContext(nil)
+	require.NoError(t, err)
+
+	group, err := createTestGroup(tdbCtx, t.TempDir())
+	require.NoError(t, err)
+
+	arraySchema := buildArraySchema(tdbCtx, t)
+	arrayPathToKeep, arrayPathToRemove := t.TempDir(), t.TempDir()
+	require.NoError(t, addTwoArraysToGroup(tdbCtx, group, arraySchema, arrayPathToKeep, arrayPathToRemove))
+
+	// verify we have two arrays
+	count, err := memberCount(group)
+	require.NoError(t, err)
+	require.EqualValues(t, 2, count)
+
+	// =========================================================================
+	// Remove the members and validate
+	require.NoError(t, group.Open(TILEDB_WRITE))
+	require.NoError(t, group.RemoveMember(arrayPathToRemove))
+	require.NoError(t, group.Close())
+
+	count, err = memberCount(group)
+	require.NoError(t, err)
+	require.EqualValues(t, uint64(1), count)
+
+	uri, objectType, err := group.GetMemberFromIndex(1)
+	require.NoError(t, err)
+	assert.EqualValues(t, uri, arrayPathToKeep)
+	assert.EqualValues(t, objectType, TILEDB_ARRAY)
+}
+
+func memberCount(group *Group) (uint64, error) {
+	conf, err := NewConfig()
+	if err != nil {
+		return 0, err
+	}
+	if err := conf.Set("sm.group.timestamp_start", strconv.Itoa(int(time.Now().UnixMilli()))); err != nil {
+		return 0, err
+	}
+
+	if err := group.SetConfig(conf); err != nil {
+		return 0, err
+	}
+
+	if err := group.Open(TILEDB_READ); err != nil {
+		return 0, err
+	}
+	count, err := group.GetMemberCount()
+	if err != nil {
+		return 0, err
+	}
+
+	if err := group.Close(); err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func createTestGroup(tdbCtx *Context, uri string) (*Group, error) {
+	// Create initial group
+	group, err := NewGroup(tdbCtx, uri)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := group.Create(); err != nil {
+		return nil, err
+	}
+	return group, nil
+}
+
+func addTwoArraysToGroup(tdbCtx *Context, group *Group, arraySchema *ArraySchema, arrayURI1, arrayURI2 string) error {
+	array1, err := NewArray(tdbCtx, arrayURI1)
+	if err != nil {
+		return err
+	}
+
+	if err := array1.Create(arraySchema); err != nil {
+		return err
+	}
+
+	array2, err := NewArray(tdbCtx, arrayURI2)
+	if err != nil {
+		return err
+	}
+
+	if err := array2.Create(arraySchema); err != nil {
+		return err
+	}
+
+	if err := group.Open(TILEDB_WRITE); err != nil {
+		return err
+	}
+
+	if err := group.AddMember(array1.uri, false); err != nil {
+		return err
+	}
+
+	if err := group.AddMember(array2.uri, false); err != nil {
+		return err
+	}
+
+	return group.Close()
 }
