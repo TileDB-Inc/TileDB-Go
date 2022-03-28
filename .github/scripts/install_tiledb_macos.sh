990f2db1eccee0f5080bb319b8af5c40eabacfa1
Stefan Naglee <stefannaglee@gmail.com>
2022-03-28 18:20:26 -0400

    Use tiledb 2.8

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.6.1/tiledb-macos-x86_64-2.6.1-2f6b7f6.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.8.0/tiledb-macos-x86_64-2.8.0-f8efd39.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
