91799ebe654ba747eee80c53b65b01132d459fe1
Stefan Naglee <stefannaglee@gmail.com>
2022-04-06 11:25:24 -0400

    Add object types

---
+++ enums_test.go
@@ -0,0 +1,21 @@
+//go:build experimental
+// +build experimental
+
+package tiledb
+
+import "testing"
+
+func TestObjectType(t *testing.T) {
+	v := TILEDB_ARRAY
+	s := v.String()
+
+	got, err := ObjectTypeFromString(s)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if got != v {
+		t.Errorf("got: %v not equal to input: %v", got, v)
+	}
+
+}
