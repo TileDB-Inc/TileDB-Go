fb1e53f83d54cce8231b851a5c4d8a7d98834599
John Kerl <kerl.john.r@gmail.com>
2022-07-08 12:59:33 -0400

    point install scripts at core 2.10.2

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.10.1/tiledb-macos-x86_64-2.10.1-6535d4c.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.10.2/tiledb-macos-x86_64-2.10.2-9ab84f9.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
