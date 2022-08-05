a58cff547115aa4a5d4d2ade7f976a6d0422e578
Seth Shelnutt <Shelnutt2@gmail.com>
2022-08-04 21:49:28 -0400

    Update to TileDB 2.10.4

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,4 +1,4 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.10.2/tiledb-linux-x86_64-2.10.2-9ab84f9.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.10.4/tiledb-linux-x86_64-2.10.4-f2b5d11.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
