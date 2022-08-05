25d47dec123af542552fb26526f69fb16b8ea438
John Kerl <kerl.john.r@gmail.com>
2022-08-05 13:09:42 -0400

    Update to tiledb core 2.11.0-rc1

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.10.4/tiledb-macos-x86_64-2.10.4-f2b5d11.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.11.0-rc1/tiledb-macos-x86_64-2.11.0-rc1-34e5dbc.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
