0ffcd441586e926d249ffd8876ae9d8276e03f35
Spyros Anastasopoulos <spyros.anastasopoulos@tiledb.com>
2023-01-23 11:27:48 +0200

    Update TileDB to 2.14.0

--- .github/scripts/install_tiledb_source_macos.sh
+++ .github/scripts/install_tiledb_source_macos.sh
@@ -1,5 +1,5 @@
 set -e -x
-git clone https://github.com/TileDB-Inc/TileDB.git -b 2.13.0
+git clone https://github.com/TileDB-Inc/TileDB.git -b 2.14.0
 cd TileDB
 mkdir build && cd build
 cmake -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
