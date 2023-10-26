791398fc40075bfcff32288abdb41ff3edd837db
Spyros Anastasopoulos <spyros.anastasopoulos@tiledb.com>
2023-10-26 16:29:12 +0300

    Update TileDB to 2.17.3

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.17.0/tiledb-macos-x86_64-2.17.0-93c173d.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.17.3/tiledb-macos-x86_64-2.17.3-0c2de58.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
