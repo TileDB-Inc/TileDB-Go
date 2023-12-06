6fe9dc22777d0ee2ea21ecf457d3e8a29b52c495
Spyros Anastasopoulos <spyros.anastasopoulos@tiledb.com>
2023-12-06 10:47:28 +0200

    Test branch with TileDB 2.19.0-rc0

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,4 +1,4 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.18.3/tiledb-linux-x86_64-2.18.3-8f8b766.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.19.0-rc0/tiledb-linux-x86_64-2.19.0-rc0-4538795c.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
