fc2e0804628e21945cc0090ace6b3b998f392ba5
Stefan Naglee <stefannaglee@gmail.com>
2023-12-19 13:13:25 -0500

    Upgrade to rc1

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.19.0-rc0/tiledb-macos-x86_64-2.19.0-rc0-4538795c.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.19.0-rc1/tiledb-macos-x86_64-2.19.0-rc1-fa30a88a.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
