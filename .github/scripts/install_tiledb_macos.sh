8478f5caabbc33fa165cbc08bb00d28d5d6f486f
Spyros Anastasopoulos <spyros.anastasopoulos@tiledb.com>
2024-01-18 10:06:26 +0200

    Update TileDB to 2.18.4

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.18.3/tiledb-macos-x86_64-2.18.3-8f8b766.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.18.4/tiledb-macos-x86_64-2.18.4-3531ce7.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
