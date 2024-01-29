414981c3004bda0336acb596af65c7958d2160d6
Spyros Anastasopoulos <spyros.anastasopoulos@tiledb.com>
2024-01-29 13:31:26 +0200

    Upgrade TileDB to 2.19.1

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,3 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.18.4/tiledb-macos-x86_64-2.18.4-3531ce7.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.19.1/tiledb-macos-x86_64-2.19.1-29ceb3e7.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
