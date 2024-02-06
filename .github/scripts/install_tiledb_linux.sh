2cf490472ecaca8e5443f0e6d939fe0af8107034
Spyros Anastasopoulos <spyros.anastasopoulos@tiledb.com>
2024-02-06 15:44:51 +0200

    Upgrade TileDB to 2.20.0-rc2

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,4 +1,4 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.19.1/tiledb-linux-x86_64-2.19.1-29ceb3e7.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.20.0-rc2/tiledb-linux-x86_64-2.20.0-rc2-40552aa.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
