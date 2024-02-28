4bb585429eefd89c337bcedad7e09107623738b9
Philipp Winter <philipp.winter@tiledb.com>
2024-02-28 09:32:13 -0600

    Add file containing TileDB version and commit ID.
    This simplifies TileDB updates because we now only have to change the
    version and commit ID in a single file.

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,4 +1,5 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.19.1/tiledb-linux-x86_64-2.19.1-29ceb3e7.tar.gz \
+source "$(dirname $0)/tiledb-version.sh"
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-linux-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
--- .github/scripts/install_tiledb_linux_debug.sh
+++ .github/scripts/install_tiledb_linux_debug.sh
@@ -1,5 +1,6 @@
 set -e -x
-git clone https://github.com/TileDB-Inc/TileDB.git -b 2.19.1
+source "$(dirname $0)/tiledb-version.sh"
+git clone https://github.com/TileDB-Inc/TileDB.git -b ${TILEDB_VERSION}
 cd TileDB
 mkdir build && cd build
 cmake -DTILEDB_WERROR=OFF -DTILEDB_VCPKG=ON -DSANITIZER=leak -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
