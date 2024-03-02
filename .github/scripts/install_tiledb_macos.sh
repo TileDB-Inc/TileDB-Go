440ebaf66549dc9a58740fc79b13d3c61ad1da2f
Philipp Winter <philipp.winter@tiledb.com>
2024-03-02 08:14:48 -0600

    Trigger a run.

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -2,3 +2,4 @@ set -e -x
 source "$(dirname $0)/tiledb-version.sh"
 curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
 && sudo tar -C /usr/local -xSf tiledb.tar.gz
+# Trigger a run
