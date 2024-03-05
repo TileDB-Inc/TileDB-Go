57a4167ebbd982732250424a6ccf38b9911eb0a9
Philipp Winter <philipp.winter@tiledb.com>
2024-03-04 20:32:38 -0600

    Test --no-overwrite-dir.

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -4,5 +4,4 @@ dl_dir="/tmp/download"
 mkdir -m 0777 -p "$dl_dir"
 curl --location -o "${dl_dir}/tiledb.tar.gz" \
     https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
-&& sudo gtar -C . -xSf "${dl_dir}/tiledb.tar.gz"
-sudo gtar -C /usr/local -xSf "${dl_dir}/tiledb.tar.gz"
+&& sudo gtar -C /usr/local --no-overwrite-dir -xSf "${dl_dir}/tiledb.tar.gz"
