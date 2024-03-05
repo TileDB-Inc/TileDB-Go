8ccc390278efbf92bd8d4636dba6649ed19b6c1d
Philipp Winter <philipp.winter@tiledb.com>
2024-03-05 08:37:21 -0600

    Try --delay-directory-restore

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -5,4 +5,4 @@ mkdir -m 0777 -p "$dl_dir"
 curl --location -o "${dl_dir}/tiledb.tar.gz" \
     https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz
 sudo tar -tvf "${dl_dir}/tiledb.tar.gz"
-sudo tar -C /usr/local -mxf "${dl_dir}/tiledb.tar.gz"
+sudo gtar -C /usr/local --delay-directory-restore -xf "${dl_dir}/tiledb.tar.gz"
