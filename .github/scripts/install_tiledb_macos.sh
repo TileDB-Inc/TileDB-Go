3d9d8e8011a240c28306b0cf324bc45012840906
Philipp Winter <philipp.winter@tiledb.com>
2024-03-05 07:33:20 -0600

    Preserve permissions

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -4,4 +4,4 @@ dl_dir="/tmp/download"
 mkdir -m 0777 -p "$dl_dir"
 curl --location -o "${dl_dir}/tiledb.tar.gz" \
     https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
-&& sudo gtar -C /usr/local --no-same-owner --no-overwrite-dir -xSf "${dl_dir}/tiledb.tar.gz"
+&& sudo gtar -C /usr/local --preserve-permissions --no-same-owner --no-overwrite-dir -xSf "${dl_dir}/tiledb.tar.gz"
