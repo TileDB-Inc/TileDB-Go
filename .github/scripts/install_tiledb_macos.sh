bd7f8911279f86a54c6ade725cd04b007545f1ce
Philipp Winter <philipp.winter@tiledb.com>
2024-03-05 08:03:08 -0600

    Try cpack tarball

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -3,5 +3,5 @@ source "$(dirname $0)/tiledb-version.sh"
 dl_dir="/tmp/download"
 mkdir -m 0777 -p "$dl_dir"
 curl --location -o "${dl_dir}/tiledb.tar.gz" \
-    https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
+    https://github.com/dudoslav/TileDB/releases/download/t05/tiledb-macos-x86_64-2.21.0.tar.gz \
 && sudo tar -C /usr/local -mxf "${dl_dir}/tiledb.tar.gz"
