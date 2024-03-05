c4dc077dac6def36c3ca3768ed004bb64da12b9a
Philipp Winter <philipp.winter@tiledb.com>
2024-03-04 19:45:52 -0600

    Use dedicated download directory.
    Apparently, tar has a tendency to occasionally fail when run inside a
    directory that is not owned by the process that runs tar.

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,4 +1,7 @@
 set -e -x
 source "$(dirname $0)/tiledb-version.sh"
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
-&& sudo tar -C /usr/local -xSf tiledb.tar.gz
+dl_dir="/tmp/download"
+mkdir -m 0777 -p "$dl_dir"
+curl --location -o "${dl_dir}/tiledb.tar.gz" \
+    https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
+&& sudo tar -C /usr/local -xSf "${dl_dir}/tiledb.tar.gz"
