0e381491283b2412b3e345ff3c4ae0d98582c293
Philipp Winter <philipp.winter@tiledb.com>
2024-03-15 16:57:54 -0500

    Simplify workflow.
    My previous PR https://github.com/TileDB-Inc/TileDB-Go/pull/292
    introduced a file that contains shared variables.  It's more idiomatic
    to put these variables directly into the workflow, using the `env`
    variable.

--- .github/scripts/install_tiledb_linux_debug.sh
+++ .github/scripts/install_tiledb_linux_debug.sh
@@ -1,6 +1,6 @@
 set -e -x
 source "$(dirname $0)/tiledb-version.sh"
-git clone https://github.com/TileDB-Inc/TileDB.git -b ${TILEDB_VERSION}
+git clone https://github.com/TileDB-Inc/TileDB.git -b ${CORE_VERSION}
 cd TileDB
 mkdir build && cd build
 cmake -DTILEDB_WERROR=OFF -DTILEDB_VCPKG=ON -DSANITIZER=leak -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,4 +1,4 @@
 set -e -x
 source "$(dirname $0)/tiledb-version.sh"
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/${TILEDB_VERSION}/tiledb-macos-x86_64-${TILEDB_VERSION}-${COMMIT_ID}.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/${CORE_VERSION}/tiledb-macos-x86_64-${CORE_VERSION}-${CORE_HASH}.tar.gz \
 && sudo tar -C /usr/local -xSf tiledb.tar.gz
