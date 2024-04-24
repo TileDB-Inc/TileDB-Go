d4beeb228d6fa232fb366bea693e53fdc10f2dcd
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2024-04-24 19:33:51 +0300

    Fix typo.

--- string.go
+++ string.go
@@ -14,7 +14,7 @@ import (
 // Converts a TileDB string handle to a Go string
 func stringHandleToString(str *C.tiledb_string_t) (string, error) {
 	var chars *C.char
-	var length C.uint64_t
+	var length C.size_t
 	ret := C.tiledb_string_view(str, &chars, &length)
 	if ret != C.TILEDB_OK {
 		return "", errors.New("could not get view of string handle")
