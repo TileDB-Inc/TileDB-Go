6bb68d93702dcba01b3ab071034fba1cc67eb119
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2024-05-29 14:23:45 +0300

    Explicitly build the `tiledb` target before installing. With the superbuild, `tiledb` will configure and build the TileDB inner project, and `install-tiledb` will build the `install` target of the inner project. Without the superbuild, `tiledb` will build the TileDB library and `install-tiledb` will alias the `install` target, so there would not be any difference when the superbuild gets removed.

--- .github/scripts/install_tiledb_linux_debug.sh
+++ .github/scripts/install_tiledb_linux_debug.sh
@@ -3,5 +3,5 @@ git clone https://github.com/TileDB-Inc/TileDB.git -b ${CORE_VERSION}
 cd TileDB
 mkdir build && cd build
 cmake -DTILEDB_WERROR=OFF -DTILEDB_VCPKG=ON -DSANITIZER=leak -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
-sudo make -j4 install-tiledb
+sudo make -j4 tiledb install-tiledb
 sudo ldconfig /usr/local/lib
--- .github/scripts/install_tiledb_source_macos.sh
+++ .github/scripts/install_tiledb_source_macos.sh
@@ -3,4 +3,4 @@ git clone https://github.com/TileDB-Inc/TileDB.git -b ${CORE_VERSION}
 cd TileDB
 mkdir build && cd build
 cmake -DTILEDB_WERROR=OFF -DTILEDB_VCPKG=ON -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
-sudo make -j$(sysctl -n hw.ncpu) install-tiledb
+sudo make -j$(sysctl -n hw.ncpu) tiledb install-tiledb
