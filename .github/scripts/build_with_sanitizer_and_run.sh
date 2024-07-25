aef4205afc93691c9f9a5d87baa0911bb6076f01
Philipp Winter <philipp.winter@tiledb.com>
2024-07-25 17:37:36 -0500

    Unbreak the address sanitizer.
    The address sanitizer has been secretly failing because we didn't
    require the job to succeed.  This commit unbreaks it, so it's actually
    doing its job.

--- .github/scripts/build_with_sanitizer_and_run.sh
+++ .github/scripts/build_with_sanitizer_and_run.sh
@@ -1,3 +1,3 @@
 set -e -x
-CGO_ENABLED=1 CGO_LDFLAGS='-fsanitize=address' CGO_CFLAGS='-fsanitize=address' go build cmd/tiledb-go/main.go
-./main
+CGO_ENABLED=1 CGO_LDFLAGS='-fsanitize=address' CGO_CFLAGS='-fsanitize=address' go build -asan ./cmd/tiledb-go-examples
+./tiledb-go-examples
