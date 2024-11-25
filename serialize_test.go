ed675827f2ab2513606d7a45787691c58d204e38
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-11-25 20:03:30 +0200

    Add test that serializes an array schema into a buffer.

--- serialize_test.go
+++ serialize_test.go
@@ -4,7 +4,62 @@ import (
 	"runtime"
 	"runtime/debug"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+type MemoryBuffer struct {
+	bytes []byte
+}
+
+func (x *MemoryBuffer) Write(buff []byte) (int, error) {
+	x.bytes = append(x.bytes, buff...)
+	return len(buff), nil
+}
+
+func TestSerializeArraySchemaGC(t *testing.T) {
+	disableGC(t)
+
+	ctx, err := NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	schema, err := NewArraySchema(ctx, TILEDB_DENSE)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dom, err := NewDomain(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dim, err := NewDimension(ctx, "d1", TILEDB_INT32, []int32{1, 10}, int32(2))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := dom.AddDimensions(dim); err != nil {
+		t.Fatal(err)
+	}
+	if err := schema.SetDomain(dom); err != nil {
+		t.Fatal(err)
+	}
+	attr, err := NewAttribute(ctx, "a1", TILEDB_INT32)
+	if err != nil {
+		t.Fatal(err)
+	}
+	schema.AddAttributes(attr)
+	buffer, err := SerializeArraySchemaToBuffer(schema, TILEDB_CAPNP, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes := &MemoryBuffer{}
+	_, err = buffer.WriteTo(bytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.NotEmpty(t, bytes.bytes)
+	runtime.GC()
+	runtime.GC()
+}
 
 func TestDeserializeArraySchemaGC(t *testing.T) {
 	disableGC(t)
