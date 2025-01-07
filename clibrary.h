ec2fd3d8de3e172b10984e94afc889643e9d0dd0
Theodore Tsirpanis <teo@tsirpanis.gr>
2025-01-07 13:09:02 +0200

    Remove unnecessary uses of `TILEDB_EXPORT`.

--- clibrary.h
+++ clibrary.h
@@ -9,25 +9,25 @@ int32_t numOfFragmentsInPath(cchar_t* path, void *data);
 int32_t vfsLs(cchar_t* path, void *data);
 int32_t objectsInPath(cchar_t* path, tiledb_object_t objectType, void *data);
 
-TILEDB_EXPORT int32_t _num_of_folders_in_path(
+int32_t _num_of_folders_in_path(
     tiledb_ctx_t* ctx,
     tiledb_vfs_t* vfs,
     const char* path,
     void* data);
 
-TILEDB_EXPORT int32_t _vfs_ls(
+int32_t _vfs_ls(
     tiledb_ctx_t* ctx,
     tiledb_vfs_t* vfs,
     const char* path,
     void* data);
 
-TILEDB_EXPORT int32_t _tiledb_object_walk(
+int32_t _tiledb_object_walk(
     tiledb_ctx_t* ctx,
     const char* path,
     tiledb_walk_order_t order,
     void* data);
 
-TILEDB_EXPORT int32_t _tiledb_object_ls(
+int32_t _tiledb_object_ls(
     tiledb_ctx_t* ctx,
     const char* path,
     void* data);
