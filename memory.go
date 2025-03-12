f6f2d65175c4c56e4f599e88252b0f5743fcf7ac
Theodore Tsirpanis <teo@tsirpanis.gr>
2025-03-12 21:56:38 +0200

    Remove dead code.

--- memory.go
+++ memory.go
@@ -1,12 +1,9 @@
 package tiledb
 
 import (
-	"runtime"
 	"unsafe"
 
 	// Much of this package relies on the fact that the Go GC is non-moving.
-	// When we move to a new Go version, this dependency should be updated
-	// to ensure that the new version is still a non-moving GC.
 	_ "go4.org/unsafe/assume-no-moving-gc"
 )
 
@@ -15,30 +12,6 @@ import (
 type Freeable interface {
 	Free() // Releases non–garbage-collected resources held by this object.
 }
-
-// freeOnGC sets a finalizer on the provided object that will cause it to
-// automatically be Free'd when it is collected by the garbage collecter.
-// It should be included immediately after the err-check of the code which
-// creates it:
-//
-//	func NewThingy() (*Thingy, error) {
-//	  thingy := Thingy{}
-//	  ret := C.tiledb_make_thingy(&thingy)
-//	  if ret != C.TILEDB_OK {
-//	    return nil, errors.New("whatever")
-//	  }
-//	  freeOnGC(&thingy)  // <-- put this here
-//	  return &thingy, nil
-//	}
-//
-// Deprecated: Use capiHandle.
-func freeOnGC(obj Freeable) {
-	runtime.SetFinalizer(obj, freeFreeable)
-}
-
-// freeFreeable frees the Freeable. It's free-floating to avoid capturing
-// anything in a closure.
-func freeFreeable(obj Freeable) { obj.Free() }
 
 // unsafeSlice creates a slice pointing at the given memory.
 func unsafeSlice[T any](ptr unsafe.Pointer, length uint) []T {
