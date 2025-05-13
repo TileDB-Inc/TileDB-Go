6766e706f975abddf594483819c5fd86f0fa3a35
Shaun Reed <shaunrd0@gmail.com>
2025-05-13 15:28:47 -0400

    Add documentation and keepalive.

--- array_schema_experimental.go
+++ array_schema_experimental.go
@@ -30,10 +30,12 @@ func NewArraySchemaAtTimestamp(tdbCtx *Context, arrayType ArrayType, timestamp u
 	return newArraySchemaFromHandle(tdbCtx, newArraySchemaHandle(arraySchemaPtr)), nil
 }
 
+// TimestampRange gets the timestamp range for the array schema.
 func (a *ArraySchema) TimestampRange() (uint64, uint64, error) {
 	var lo C.uint64_t
 	var hi C.uint64_t
 	ret := C.tiledb_array_schema_timestamp_range(a.context.tiledbContext.Get(), a.tiledbArraySchema.Get(), &lo, &hi)
+	runtime.KeepAlive(a)
 	if ret != C.TILEDB_OK {
 		return 0, 0, fmt.Errorf("error getting timestamp range: %w", a.context.LastError())
 	}
