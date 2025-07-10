3a45933202669eef486f0d008747db3e45acd1ce
Shaun Reed <shaunrd0@gmail.com>
2025-07-10 16:14:08 -0400

    Add checksums.

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,5 +1,9 @@
 set -e -x
 # TODO: Revert change to use fork release from shaunrd0/TileDB.
-curl --location -o tiledb.tar.gz https://github.com/shaunrd0/TileDB/releases/download/${CORE_VERSION}/tiledb-linux-x86_64-${CORE_VERSION}-${CORE_HASH}.tar.gz \
-&& sudo tar -C /usr/local -xf tiledb.tar.gz
+curl --location -o tiledb.tar.gz https://github.com/shaunrd0/TileDB/releases/download/${CORE_VERSION}/tiledb-linux-x86_64-${CORE_VERSION}-${CORE_HASH}.tar.gz
+echo "3ee69238b3e91dbe5c3b935bd0617ba3c3a5c03559815106360c2fdab5618e94 tiledb.tar.gz" >> checksums.txt
+sha256sum -c checksums.txt
+rm checksums.txt
+
+sudo tar -C /usr/local -xf tiledb.tar.gz
 sudo ldconfig /usr/local/lib
--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,4 +1,7 @@
 set -e -x
 # TODO: Revert change to use fork release from shaunrd0/TileDB.
-curl --location -o tiledb.tar.gz https://github.com/shaunrd0/TileDB/releases/download/${CORE_VERSION}/tiledb-macos-arm64-${CORE_VERSION}-${CORE_HASH}.tar.gz \
-&& sudo tar -C /usr/local -xSf tiledb.tar.gz
+curl --location -o tiledb.tar.gz https://github.com/shaunrd0/TileDB/releases/download/${CORE_VERSION}/tiledb-macos-arm64-${CORE_VERSION}-${CORE_HASH}.tar.gz
+echo "777be952c51e7eaba95029e8d439d72cd008724f34c3bd9f741a51cc57663a2c tiledb.tar.gz" >> checksums.txt
+sha256sum -c checksums.txt
+rm checksums.txt
+sudo tar -C /usr/local -xSf tiledb.tar.gz
