372a80ba403e67558af9cc8d9774dbafa91257b1
Shaun Reed <shaunrd0@gmail.com>
2025-07-10 15:05:44 -0400

    Use patched core release.

--- .github/scripts/install_tiledb_linux_debug.sh
+++ .github/scripts/install_tiledb_linux_debug.sh
@@ -1,5 +1,6 @@
 set -e -x
-git clone https://github.com/TileDB-Inc/TileDB.git -b ${CORE_VERSION}
+# TODO: Revert change to use fork release from shaunrd0/TileDB.
+git clone https://github.com/shaunrd0/TileDB.git -b ${CORE_VERSION}
 cd TileDB
 mkdir build && cd build
 cmake -DTILEDB_WERROR=OFF -DTILEDB_VCPKG=ON -DSANITIZER=leak -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
