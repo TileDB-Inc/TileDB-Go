2e9b1ad4468c733799191cf5ce592ca49973a77b
Shaun Reed <shaunrd0@gmail.com>
2025-07-10 15:05:44 -0400

    Use patched core release.

--- .github/scripts/install_tiledb_macos.sh
+++ .github/scripts/install_tiledb_macos.sh
@@ -1,3 +1,4 @@
 set -e -x
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/${CORE_VERSION}/tiledb-macos-arm64-${CORE_VERSION}-${CORE_HASH}.tar.gz \
+# TODO: Revert change to use fork release from shaunrd0/TileDB.
+curl --location -o tiledb.tar.gz https://github.com/shaunrd0/TileDB/releases/download/${CORE_VERSION}/tiledb-macos-arm64-${CORE_VERSION}-${CORE_HASH}.tar.gz \
 && sudo tar -C /usr/local -xSf tiledb.tar.gz
