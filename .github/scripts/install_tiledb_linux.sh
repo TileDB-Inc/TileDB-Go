cec0ff94a8341b03fc3574af81ce5d9c9ae2294c
Shaun Reed <shaunrd0@gmail.com>
2025-07-11 15:54:41 -0400

    Add linear story to comment.

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,5 +1,6 @@
 set -e -x
 # TODO: Revert change to use fork release from shaunrd0/TileDB.
+# https://linear.app/tiledb/issue/CLOUD-1043/revert-patched-core-version-used-in-tiledb-server
 curl --location -o tiledb.tar.gz https://github.com/shaunrd0/TileDB/releases/download/${CORE_VERSION}/tiledb-linux-x86_64-${CORE_VERSION}-${CORE_HASH}.tar.gz
 echo "3ee69238b3e91dbe5c3b935bd0617ba3c3a5c03559815106360c2fdab5618e94 tiledb.tar.gz" >> checksums.txt
 sha256sum -c checksums.txt
