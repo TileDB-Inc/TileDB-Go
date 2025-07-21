a20207d5e561aba468d84f54c3987ce597b96f16
Shaun Reed <shaunrd0@gmail.com>
2025-07-21 11:21:40 -0400

    Test 2.28.1-rc1.

--- .github/scripts/install_tiledb_linux_debug.sh
+++ .github/scripts/install_tiledb_linux_debug.sh
@@ -1,7 +1,5 @@
 set -e -x
-# TODO: Revert change to use fork release from shaunrd0/TileDB.
-# https://linear.app/tiledb/issue/CLOUD-1043/revert-patched-core-version-used-in-tiledb-server
-git clone https://github.com/shaunrd0/TileDB.git -b ${CORE_VERSION}
+git clone https://github.com/TileDB-Inc/TileDB.git -b ${CORE_VERSION}
 cd TileDB
 mkdir build && cd build
 cmake -DTILEDB_WERROR=OFF -DTILEDB_VCPKG=ON -DSANITIZER=leak -DTILEDB_VERBOSE=OFF -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
